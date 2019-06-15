031827adc8b0d43d01118182e164b96acd3985fe
young1lin <40629321+young1lin@users.noreply.github.com>
2019-06-16 01:13:49 +0800

    init project

---
+++ eureka-server/src/main/java/com/disdispace/hello/HelloApplication.java
@@ -0,0 +1,15 @@
+package com.disdispace.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class HelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApplication.class, args);
+	}
+
+}
